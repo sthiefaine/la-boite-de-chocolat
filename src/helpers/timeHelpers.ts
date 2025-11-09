8bc393a3e270d76873d489b89a84a47076c67f3f
thief <thiefainedev@gmail.com>
2025-11-09 22:35:33 +0100

    fix(import): allowed hours import

--- src/helpers/timeHelpers.ts
+++ src/helpers/timeHelpers.ts
@@ -7,7 +7,7 @@ export const getParisCurrentTime = async (): Promise<Date> => {
   return new Date(currentTime);
 };
 
-export const allowedHours = [0, 1, 8, 12, 17];
+export const allowedHours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
 export const shouldRunImport = async (): Promise<boolean> => {
   const parisTime = await getParisCurrentTime();
